d325e1218a5701cb497b18c23cd719a94583d061
McEileen <email": null>
2015-11-15 17:59:53 -0600

    display prof page w/o errors, need to add formatting

--- my-runners-keeper/app/controllers/runners_controller.rb
+++ my-runners-keeper/app/controllers/runners_controller.rb
@@ -2,6 +2,9 @@ class RunnersController < ApplicationController
 
   def show
     @runner = Runner.find_by(id: params[:id])
+    p "+++++++++++++++++++++++++++++++++++++++++++++++"
+    p @runner
+    p params
     @runs = @runner.runs
   end
 
