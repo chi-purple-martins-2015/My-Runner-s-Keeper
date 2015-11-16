da6647915d44f3ddf544d398e7553b077cf915c6
McEileen <email": null>
2015-11-15 18:24:03 -0600

    get link to all runners on index

--- my-runners-keeper/app/controllers/runners_controller.rb
+++ my-runners-keeper/app/controllers/runners_controller.rb
@@ -2,10 +2,11 @@ class RunnersController < ApplicationController
 
   def show
     @runner = Runner.find_by(id: params[:id])
-    p "+++++++++++++++++++++++++++++++++++++++++++++++"
-    p @runner
-    p params
     @runs = @runner.runs
+  end
+
+  def index
+    @runners = Runner.all
   end
 
 end
