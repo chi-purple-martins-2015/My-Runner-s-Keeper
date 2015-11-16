0ad4c876aa4d343e4dc10c26f24c2a55c86eb023
McEileen <email": null>
2015-11-15 18:39:39 -0600

    include registration/login/logout stuff in sessions and runners controller that I don't all understand

--- my-runners-keeper/app/controllers/runners_controller.rb
+++ my-runners-keeper/app/controllers/runners_controller.rb
@@ -1,5 +1,19 @@
 class RunnersController < ApplicationController
 
+  def new
+    @runner = Runner.new
+  end
+
+  def create
+    @runner = Runner.new(runner_params)
+    if @runner.save
+      redirect_to "/"
+    else
+      render "new"
+    end
+  end
+
+
   def show
     @runner = Runner.find_by(id: params[:id])
     @runs = @runner.runs
@@ -8,5 +22,12 @@ class RunnersController < ApplicationController
   def index
     @runners = Runner.all
   end
+
+  private
+
+  def runner_params
+    params.require(:runner).permit(:first_name, :last_name, :captain, :email, :img_url, :password)
+  end
+
 
 end
