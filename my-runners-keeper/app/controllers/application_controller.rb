bf8264fc0c49f1107674d4919b2d3354e1c1b221
McEileen <email": null>
2015-11-15 18:29:17 -0600

    Create application controller and helper methods for sessions

--- my-runners-keeper/app/controllers/application_controller.rb
+++ my-runners-keeper/app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_runner
+    @current_runner ||= Runner.find_by(id: session[:runner_id]) if session[:runner_id]
+  end
+
+  def logged_in?
+    session[:runner_id].present?
+  end
+
+  helper_method :current_runner
+  helper_method :logged_in?
+
 end
